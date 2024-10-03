131c7af5c2ecfeba957283368dab93c0e6b3fdee
Faris ibrahim <Faris@gmail.com>
2024-10-04 02:02:11 +0300

    Special ( Calculator App ) Files in 5/OCT/2024 at 2 AM

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+  if (btnValue === "=" && btnValue !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+// Add event listener to buttons , call calculate() on click.
+buttons.forEach((button) => {
+  // button click listener calls calculate() with dataset value argument
+  button.addEventListener("click", (button) =>
+    calculate(button.target.dataset.value)
+  );
+});
